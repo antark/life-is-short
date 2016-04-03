f41771a6039aed79ab00cfb6cbe95c9f232f35d3
antark <ants.arks@gmail.com>
2016-04-03 08:28:50 +0800

    golang list 排序修改

--- golang/list_ext/sorted_list.go
+++ golang/list_ext/sorted_list.go
@@ -1,23 +1,48 @@
-// sorted_list_test.go
+// sorted_list.go
 package sort_ext
 
 import (
 	"container/list"
-	"fmt"
-	"testing"
+	"sort"
 )
 
-func TestSortedList(t *testing.T) {
-	origin := [...]uint64{1, 7, 3, 5, 2, 8, 6, 4, 9}
-	var data list.List
-	for _, value := range origin {
-		data.PushBack(value)
+type SortedList struct {
+	Value *list.List
+	data  []*list.Element
+	Cmp   func(a, b *interface{}) bool
+}
+
+func (slist *SortedList) Init() {
+	for ele := slist.Value.Front(); ele != nil; ele = ele.Next() {
+		slist.data = append(slist.data, ele)
 	}
-	SortList(&data, func(a, b *interface{}) bool {
-		return (*a).(uint64) < (*b).(uint64)
-	})
-	for ele := data.Front(); ele != nil; ele = ele.Next() {
-		fmt.Printf("%d ", ele.Value) // ele.Value ==> (*ele).Value
+}
+
+func (slist *SortedList) Post() {
+	slist.Value.Init()
+	for _, value := range slist.data {
+		slist.Value.PushBack(value.Value)
 	}
-	fmt.Println()
+}
+
+func (slist SortedList) Len() int {
+	return len(slist.data)
+}
+
+func (slist SortedList) Swap(i, j int) {
+	slist.data[i], slist.data[j] = slist.data[j], slist.data[i]
+}
+
+func (slist SortedList) Less(i, j int) bool {
+	return slist.Cmp(&slist.data[i].Value, &slist.data[j].Value)
+}
+
+func SortList(data *list.List, cmp func(a, b *interface{}) bool) {
+	if data == nil || cmp == nil {
+		panic("error: input list is nil, or cmp is nil")
+	}
+	var slist SortedList = SortedList{Value: data, Cmp: cmp}
+	slist.Init()
+	sort.Sort(slist)
+	slist.Post()
 }
