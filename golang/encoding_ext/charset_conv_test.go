b1096a3b604a4080958437d76dc15101075daf8f
文琼 <ants.arks@gmail.com>
2016-08-12 21:15:57 +0800

    Update charset_conv_test.go

--- golang/encoding_ext/charset_conv_test.go
+++ golang/encoding_ext/charset_conv_test.go
@@ -7,14 +7,17 @@ import (
 
 func TestCharsetConv(t *testing.T) {
 	origin := []byte("你好，世界!")
+	fmt.Println(string(origin))
 
 	gbk, err := PutBytes(origin, "GB18030")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(origin))
 	fmt.Println(string(gbk))
 
 	utf8, err := GetBytes(gbk, "GBK")
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(string(utf8))
 }
