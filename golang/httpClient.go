31f8d0be32a41c6a57af560e0f6c2ac2d1b117fc
antark <ants.arks@gmail.com>
2016-08-13 15:29:35 +0800

    file format

--- golang/httpClient.go
+++ golang/httpClient.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/antark/golang-x/encoding_ext"
+	"github.com/antark/golang-x/encoding_ext"
 )
 
 func errCheck(err error) {
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-    // do request
-    url := "http://www.szse.cn/szseWeb/common/szse/files/text/jy/jy160811.txt"
-    req, err := http.NewRequest("GET", url, nil)
-    errCheck(err)
+	// do request
+	url := "http://www.szse.cn/szseWeb/common/szse/files/text/jy/jy160811.txt"
+	req, err := http.NewRequest("GET", url, nil)
+	errCheck(err)
 
-    client := http.DefaultClient
-    resp, err := client.Do(req)
-    errCheck(err)
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	errCheck(err)
 
-    // process response body
-    text, err := ioutil.ReadAll(resp.Body)
-    errCheck(err)
+	// process response body
+	text, err := ioutil.ReadAll(resp.Body)
+	errCheck(err)
 
-    // charset conversion: GBK --> UTF-8
-    utfText, err := encoding_ext.GetBytes(text, "GBK")
-    errCheck(err)
+	// charset conversion: GBK --> UTF-8
+	utfText, err := encoding_ext.GetBytes(text, "GBK")
+	errCheck(err)
 
-    log.Print(string(utfText))
+	log.Print(string(utfText))
 
-    // store
-    file, err := os.Create("sz-jy-160811.txt")
-    errCheck(err)
+	// store
+	file, err := os.Create("sz-jy-160811.txt")
+	errCheck(err)
 
-    _, err = file.Write(utfText)
-    errCheck(err)
+	_, err = file.Write(utfText)
+	errCheck(err)
 
-    return
+	return
 }
